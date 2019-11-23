55095d2baa54630388dd957d3aee2296f8ee9b95
chenjun <675816156@qq.com>
2019-11-23 05:36:40 -0800

    raspi4b firmware boot configuration

--- update_firmware_rpi4.sh
+++ update_firmware_rpi4.sh
@@ -1,6 +1,19 @@
-unzip rpi-boot-eeprom-recovery-2019-09-10-vl805-000137ab.zip
-./rpi-eeprom/rpi-eeprom-config ./pieeprom.bin >bootconf.txt
+# unzip rpi-boot-eeprom-recovery-2019-09-10-vl805-000137ab.zip
+git submodule init
+git submodule sync
+git submodule update
+
+pushd rpi-eeprom/
+git pull origin master
+popd
+
+newest_firmware=$(ls rpi-eeprom/firmware/beta/pieeprom-* | gawk 'END { print $0 }')
+newest_vl805=$(ls rpi-eeprom/firmware/beta/vl805-* | gawk 'END { print $0 }')
+
+./rpi-eeprom/rpi-eeprom-config ${newest_firmware} >bootconf.txt
 vim bootconf.txt
-./rpi-eeprom/rpi-eeprom-config --out pieeprom_new.bin --config bootconf.txt pieeprom.bin
-sha256sum pieeprom_new.bin | awk '{print $1}' > pieeprom.sig
-mv pieeprom_new.bin pieeprom.bin
+./rpi-eeprom/rpi-eeprom-config --out pieeprom.bin --config bootconf.txt ${newest_firmware}
+sha256sum pieeprom.bin | awk '{print $1}' > pieeprom.sig
+cp ${newest_vl805} vl805.bin
+sha256sum vl805.bin | awk '{print $1}' > vl805.sig
+cp ./rpi-eeprom/firmware/beta/recovery.bin .
